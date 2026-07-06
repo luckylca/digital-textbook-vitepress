77c108c586911e380f98b0d16c7aff9afa903522
lucky <101040016+luckylca@users.noreply.github.com>
2026-07-06 16:44:00 +0800

    添加例题组件

---
+++ docs/.vitepress/theme/components/ExampleBlock.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <n-card class="teaching-card example-block" embedded>
+    <template #header>
+      <div class="example-block__title">{{ title }}</div>
+    </template>
+    <div class="example-block__content">
+      <slot />
+    </div>
+  </n-card>
+</template>
+
+<style scoped>
+.example-block {
+  margin: 18px 0;
+}
+
+.example-block__title {
+  font-weight: 700;
+}
+
+.example-block__content {
+  line-height: 1.8;
+}
+</style>
